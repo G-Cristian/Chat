b58bc6858a6d4db818f2b1720242b9b5347eda15
CGalli <cgalli@dc.uba.ar>
2019-06-26 11:19:04 -0300

    Added a template version of print and printErr

--- Include/debugger.h
+++ Include/debugger.h
@@ -13,8 +13,12 @@ public:
     static void dispose();
     ~Debugger();
 
+    template<typename T>
+    void print(const T&);
     void print(const std::string&);
     void print(const char*);
+    template<typename T>
+    void printErr(const T&);
     void printErr(const std::string&);
     void printErr(const char*);
 private:
@@ -26,5 +30,19 @@ private:
     Mutex _stdOutMutex;
     Mutex _errOutMutex;
 };
+
+template<typename T>
+void Debugger::print(const T &msg){
+    ::lock(&_stdOutMutex);
+    _stdOut << msg << std::endl;
+    ::unlock(&_stdOutMutex);
+}
+
+template<typename T>
+void Debugger::printErr(const T &msg){
+    ::lock(&_errOutMutex);
+    _errOut << msg << std::endl;
+    ::unlock(&_errOutMutex);
+}
 
 #endif
