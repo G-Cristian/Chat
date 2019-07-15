72503f3d6a4508ddb1f87c2a89da05a0e7d47af5
G-Cristian <registro.paginas2@gmail.com>
2019-07-15 10:19:55 -0300

    Added support for windows connections.

--- Include/connection.h
+++ Include/connection.h
@@ -1,36 +1,86 @@
 #ifndef _CONNECTION_H_
 #define _CONNECTION_H_
 
+#if defined(__linux__) || defined(__unix__)
+//linux based
 #include <arpa/inet.h>
-#include <cstring>
-#include <memory>
 #include <netinet/in.h>
-#include <string>
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <unistd.h>
+#elif defined(_WIN32) || defined(WIN32)
+//windows based
+#ifndef WIN32_LEAN_AND_MEAN
+#define WIN32_LEAN_AND_MEAN
+#endif
+
+#include <windows.h>
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <iphlpapi.h>
+
+#pragma comment (lib, "Ws2_32.lib")
+#pragma comment (lib, "Mswsock.lib")
+#pragma comment (lib, "AdvApi32.lib")
+#else
+#error "OS not supperted."
+#endif
+
+#include <cstring>
+#include <memory>
+#include <string>
 #include <utility>
 
 struct Connection{
+#if defined(__linux__) || defined(__unix__)
+	//linux based
     typedef int SOCKET;
     typedef uint16_t PORT;
     typedef struct sockaddr_in SOCKADDR;
     typedef socklen_t SOCKLEN;
+	using INVALID_SOCKET = -1;
+#elif defined(_WIN32) || defined(WIN32)
+	//windows based
+	typedef SOCKET SOCKET;
+	typedef uint16_t PORT;
+	typedef sockaddr SOCKADDR;
+	typedef size_t SOCKLEN;
+	//using INVALID_SOCKET = INVALID_SOCKET;
+#else
+#error "OS not supperted."
+#endif
 
+#if defined(_WIN32) || defined(WIN32)
+	WSADATA wsaData;
+#endif
     PORT port;
     SOCKET endpoint;
     SOCKADDR sockaddr;
     SOCKLEN sockaddrLen;
 
-    Connection():port(0),endpoint(-1),sockaddrLen(0){
+    Connection():port(0),endpoint(INVALID_SOCKET),sockaddrLen(0){
         std::memset((void*)(&sockaddr), 0, sizeof(SOCKADDR));
     }
 
     ~Connection(){
-        if(endpoint != -1){
+#if defined(__linux__) || defined(__unix__)
+		//linux based
+        if(endpoint != INVALID_SOCKET){
             close(endpoint);
-            endpoint = -1;
+            endpoint = INVALID_SOCKET;
         }
+#elif defined(_WIN32) || defined(WIN32)
+		//windows based
+		if (endpoint != INVALID_SOCKET)
+		{
+			shutdown(endpoint, SD_SEND);
+			closesocket(endpoint);
+			WSACleanup();
+			endpoint = INVALID_SOCKET;
+		}
+#else
+#error "OS not supperted."
+#endif
     }
 
     int listen() const;
@@ -42,33 +92,52 @@ struct Connection{
 };
 
 template<int N>
-int Connection::sendMsg(const char msg[N])const{
-    int len = N;
-    int sent = 0;
-    const char *str = msg;
-    do{
-        len -= sent;
-        str += sent;
-        sent = send(endpoint, str, len, 0);
-        if(sent == -1){
-            return 0;
-        }
-    }while(sent != len);
+int Connection::sendMsg(const char msg[N])const {
+	int len = N;
+	int sent = 0;
+	const char *str = msg;
+#if defined(__linux__) || defined(__unix__)
+	do {
+		len -= sent;
+		str += sent;
+		sent = send(endpoint, str, len, 0);
+		if (sent == -1) {
+			return 0;
+		}
+	} while (sent != len);
 
-    return 1;
+	return 1;
+#elif defined(_WIN32) || defined(WIN32)
+	int iResult = send(endpoint, str, N, 0);
+	if (iResult == SOCKET_ERROR) {
+		return 0;
+	}
+	return 1;
+#else
+#error "OS not supperted."
+#endif
 }
 
 template<int N>
 std::pair<int, std::shared_ptr<char>> Connection::rcvMsg()const{
     char *buff = (char*)malloc(N*sizeof(char));
-
-    int recvLen = 0;
+	int recvLen = 0;
+#if defined(__linux__) || defined(__unix__)
     recvLen=recv(endpoint, (void*)buff,N,0);
    // if(recvLen == -1){
    //     return {-1, ""};
    // }
+#elif defined(_WIN32) || defined(WIN32)
+	recvLen = recv(endpoint, buff, N, 0);
+	if (recvLen < 0) {
+		recvLen = -1;
+	}
+#else
+#error "OS not supperted."
+#endif
    std::shared_ptr<char> mg(buff, [](char*p){free(p);});
-    return {recvLen, mg};
+
+   return {recvLen, mg};
 }
 
 class ConnectionFactory{
--- Source/connection.cpp
+++ Source/connection.cpp
@@ -1,14 +1,25 @@
 #include "../Include/connection.h"
+#include <sstream>
 
 int Connection::listen() const{
+#if defined(__linux__) || defined(__unix__)
     if(::listen(endpoint, 50) == -1){
         throw "Cannot listen.";
     }
-
+#elif defined(_WIN32) || defined(WIN32)
+	if (::listen(endpoint, SOMAXCONN) == SOCKET_ERROR) {
+		std::stringstream ss;
+		ss << "listen failed with error: " << WSAGetLastError() << std::endl;
+		throw ss.str();
+	}
+#else
+#error "OS not supperted."
+#endif
     return 1;
 }
 
 std::shared_ptr<Connection> Connection::acceptConnection()const{
+#if defined(__linux__) || defined(__unix__)
     std::shared_ptr<Connection> connection = std::make_shared<Connection>();
     connection->port = port;
     connection->sockaddrLen = sizeof(struct sockaddr_in);
@@ -16,11 +27,36 @@ std::shared_ptr<Connection> Connection::acceptConnection()const{
     if(connection->endpoint == -1){
         throw "Error accept.";
     }
+#elif defined(_WIN32) || defined(WIN32)
+	//windows based
+	std::shared_ptr<Connection> connection = std::make_shared<Connection>();
+	connection->port = port;
+	connection->endpoint = INVALID_SOCKET;
 
+	int iResult = WSAStartup(MAKEWORD(2, 2), &(connection->wsaData));
+	if (iResult != 0) {
+		std::stringstream ss;
+		ss << "WSAStartup failed with error: " << iResult << std::endl;
+		throw ss.str();
+	}
+
+	connection->sockaddrLen = sizeof(struct sockaddr_in);
+	connection->endpoint = accept(endpoint, &(connection->sockaddr), (int*)&(connection->sockaddrLen));
+	// Accept a client socket
+	if (connection->endpoint == INVALID_SOCKET) {
+		WSACleanup();
+		std::stringstream ss;
+		ss << "accept failed with error: " << WSAGetLastError() << std::endl;
+		throw ss.str();
+	}
+#else
+#error "OS not supperted."
+#endif
     return connection;
 }
 
 std::shared_ptr<Connection> ConnectionFactory::createServerConnection(PORT port) const{
+#if defined(__linux__) || defined(__unix__)
     struct in_addr inAddr;
     inAddr.s_addr = INADDR_ANY;
 
@@ -44,11 +80,75 @@ std::shared_ptr<Connection> ConnectionFactory::createServerConnection(PORT port)
     if(bind(endpoint, (struct sockaddr*)&(connection->sockaddr), connection->sockaddrLen) == -1){
         throw "Cannot bind.";
     }
+#elif defined(_WIN32) || defined(WIN32)
+	//windows based
+	std::shared_ptr<Connection> connection = std::make_shared<Connection>();
+	connection->port = port;
+	connection->endpoint = INVALID_SOCKET;
+	struct addrinfo *result = NULL;
+	struct addrinfo hints;
 
+	// Initialize Winsock
+	int iResult = WSAStartup(MAKEWORD(2, 2), &(connection->wsaData));
+	if (iResult != 0) {
+		std::stringstream ss;
+		ss << "WSAStartup failed with error: " << iResult << std::endl;
+		throw ss.str();
+	}
+
+	ZeroMemory(&hints, sizeof(hints));
+	hints.ai_family = AF_INET;
+	hints.ai_socktype = SOCK_STREAM;
+	hints.ai_protocol = IPPROTO_TCP;
+	hints.ai_flags = AI_PASSIVE;
+
+	// Resolve the server address and port
+	std::stringstream ss;
+	ss << port;
+	iResult = getaddrinfo(NULL, ss.str().c_str(), &hints, &result);
+	if (iResult != 0) {
+		WSACleanup();
+		std::stringstream ss;
+		ss << "getaddrinfo failed with error: " << iResult << std::endl;
+		throw ss.str();
+	}
+
+	// Create a SOCKET for connecting to server
+	connection->endpoint = socket(result->ai_family, result->ai_socktype, result->ai_protocol);
+	if (connection->endpoint == INVALID_SOCKET) {
+		freeaddrinfo(result);
+		WSACleanup();
+		std::stringstream ss;
+		ss << "socket failed with error: " << WSAGetLastError() << std::endl;
+		throw ss.str();
+	}
+
+	connection->sockaddr = *(result->ai_addr);
+	connection->sockaddrLen = result->ai_addrlen;
+
+	// Setup the TCP listening socket
+	iResult = bind(connection->endpoint, (sockaddr*)&(connection->sockaddr), (int)connection->sockaddrLen);
+	if (iResult == SOCKET_ERROR) {
+		freeaddrinfo(result);
+		closesocket(connection->endpoint);
+		connection->endpoint = INVALID_SOCKET;
+		WSACleanup();
+		std::stringstream ss;
+		ss << "bind failed with error: " << WSAGetLastError() << std::endl;
+		throw ss.str();
+	}
+
+	freeaddrinfo(result);
+
+#else
+#error "OS not supperted."
+#endif
     return connection;
 }
 
 std::shared_ptr<Connection> ConnectionFactory::createClientConnection(PORT port, const std::string &ip) const{
+#if defined(__linux__) || defined(__unix__)
+	//linux based
     struct in_addr inAddr;
     int ok = inet_aton(ip.c_str(), &inAddr);
     if(!ok){
@@ -76,6 +176,76 @@ std::shared_ptr<Connection> ConnectionFactory::createClientConnection(PORT port,
     if(connect(endpoint, (struct sockaddr*)&(connection->sockaddr), connection->sockaddrLen) == -1){
         throw "Error connecting to server.";
     }
+#elif defined(_WIN32) || defined(WIN32)
+	//windows based
+	std::shared_ptr<Connection> connection = std::make_shared<Connection>();
+	connection->port = port;
+	connection->endpoint = INVALID_SOCKET;
+	struct addrinfo *result = NULL,
+		*ptr = NULL,
+		hints;
+
+	// Initialize Winsock
+	int iResult = WSAStartup(MAKEWORD(2, 2), &(connection->wsaData));
+	if (iResult != 0) {
+		std::stringstream ss;
+		ss << "WSAStartup failed with error: " << iResult << std::endl;
+		throw ss.str();
+	}
+
+	ZeroMemory(&hints, sizeof(hints));
+	hints.ai_family = AF_UNSPEC;
+	hints.ai_socktype = SOCK_STREAM;
+	hints.ai_protocol = IPPROTO_TCP;
+
+	// Resolve the server address and port
+	std::stringstream ss;
+	ss << port;
+	iResult = getaddrinfo(ip.c_str(), ss.str().c_str(), &hints, &result);
+	if (iResult != 0) {
+		WSACleanup();
+		std::stringstream ss;
+		ss << "getaddrinfo failed with error: " << iResult << std::endl;
+		throw ss.str();
+	}
+
+	// Attempt to connect to an address until one succeeds
+	for (ptr = result; ptr != NULL;ptr = ptr->ai_next) {
+
+		// Create a SOCKET for connecting to server
+		connection->endpoint = socket(ptr->ai_family, ptr->ai_socktype,
+			ptr->ai_protocol);
+		if (connection->endpoint == INVALID_SOCKET) {
+			freeaddrinfo(result);
+			WSACleanup();
+			std::stringstream ss;
+			ss << "socket failed with error: " << WSAGetLastError() << std::endl;
+			throw ss.str();
+		}
+
+		connection->sockaddr = *(ptr->ai_addr);
+		connection->sockaddrLen = ptr->ai_addrlen;
+
+		// Connect to server.
+		iResult = connect(connection->endpoint, (struct sockaddr*)&(connection->sockaddr), (int)connection->sockaddrLen);
+		if (iResult == SOCKET_ERROR) {
+			closesocket(connection->endpoint);
+			connection->endpoint = INVALID_SOCKET;
+			continue;
+		}
+		break;
+	}
+
+	freeaddrinfo(result);
+
+	if (connection->endpoint == INVALID_SOCKET) {
+		WSACleanup();
+		throw "Unable to connect to server!";
+	}
+
+#else
+#error "OS not supperted."
+#endif
 
     return connection;
 }
